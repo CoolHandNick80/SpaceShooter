10a955ddc4166adf4501dae072e0dd64f29f8697
CoolHandNick80 <niccol80@hotmail.com>
2019-06-03 17:25:34 +0200

    Main Menu nav

--- Source/SpaceShooter/Widgets/MainMenuWidget.h
+++ Source/SpaceShooter/Widgets/MainMenuWidget.h
@@ -4,9 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/InputComponent.h "
 #include "MainMenuWidget.generated.h"
 
+
 class UButton;
+class APlayerController;
 
 /**
  * 
@@ -16,7 +19,56 @@ class SPACESHOOTER_API UMainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
-		UPROPERTY()
-		TArray<UButton> ButtonsArray;
+	virtual void NativeConstruct() override;
+
+	//virtual void InitializeInputComponent() override;
+
+	//virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+protected:
+
+	UPROPERTY()
+		int32 PreviousButtonIndex = 0;
+
+	UPROPERTY()
+		int32 CurrentButtonIndex = 0;
+
+	UPROPERTY()
+		int32 LastArrayIndex = 0;
+
+	UPROPERTY()
+		APlayerController* PlayerController = nullptr;
+
+	UPROPERTY()
+		TArray<UButton*> ButtonsArray;
 	
+	UPROPERTY()
+		UButton* NewGameButton = nullptr;
+
+	UPROPERTY()
+		UButton* ContinueButton = nullptr;
+
+	UPROPERTY()
+		UButton* LoadGameButton = nullptr;
+
+	UPROPERTY()
+		UButton* QuitButton = nullptr;
+
+	
+		FInputBinding InputBinding = FInputBinding::FInputBinding();
+ 
+public:
+
+	float StickX = 0.F;
+	
+	float StickY = 0.F;
+
+protected:
+
+	void SetupData();
+
+	UFUNCTION()
+		void ScrollButtons(float InValue);
+
+
 };
