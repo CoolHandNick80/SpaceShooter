aeaff38d9874d2e57bb57d9e9fa88773001ef237
CoolHandNick80 <niccol80@hotmail.com>
2019-06-05 12:56:54 +0200

    change

--- Source/SpaceShooter/Widgets/MainMenuWidget.cpp
+++ Source/SpaceShooter/Widgets/MainMenuWidget.cpp
@@ -2,4 +2,105 @@
 
 
 #include "MainMenuWidget.h"
+#include "Runtime/UMG/Public/Blueprint/WidgetTree.h"
+#include "Widgets/MyButton.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerController.h"
+
+
+void UMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SetupData();
+	
+	NewGameButton->SetKeyboardFocus();
+
+	CicleArray();
+
+	//InputBinding = PlayerController->InputComponent->BindAxis("MenuNav", this, &UMainMenuWidget::ScrollButtons);
+	//InputBinding.bConsumeInput = false;
+}
+
+void UMainMenuWidget::SetupData()
+{
+	PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	verify(PlayerController != nullptr);
+
+	NewGameButton = Cast<UMyButton>(WidgetTree->FindWidget("BTN_NewGame"));
+	verify(NewGameButton != nullptr);
+
+	if (NewGameButton != nullptr)
+	{
+		ButtonsArray.Add(NewGameButton);
+	}
+
+	ContinueButton = Cast<UMyButton>(WidgetTree->FindWidget("BTN_Continue"));
+
+	if (ContinueButton != nullptr)
+	{
+		ButtonsArray.Add(ContinueButton);
+	}
+
+	LoadGameButton = Cast<UMyButton>(WidgetTree->FindWidget("BTN_LoadGame"));
+
+	if (LoadGameButton != nullptr)
+	{
+		ButtonsArray.Add(LoadGameButton);
+	}
+
+	QuitButton = Cast<UMyButton>(WidgetTree->FindWidget("BTN_Quit"));
+
+	if (QuitButton != nullptr)
+	{
+		ButtonsArray.Add(QuitButton);
+	}
+
+	LastArrayIndex  = (ButtonsArray.Num()-1);
+
+	//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::FromInt(LastArrayIndex));
+}
+
+void UMainMenuWidget::ScrollButtons(float InValue)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Green, FString::SanitizeFloat(InValue));
+
+	if (!FMath::IsNearlyZero(InValue, 0.3F))
+	{
+		if (InValue > 0.7F)
+		{
+			if (CurrentButtonIndex > 0)
+			{
+				PreviousButtonIndex = CurrentButtonIndex;
+				CurrentButtonIndex -= 1;
+			}
+		}
+
+		if (InValue < -0.7F)
+		{
+			if (CurrentButtonIndex < LastArrayIndex)
+			{
+				PreviousButtonIndex = CurrentButtonIndex;
+				CurrentButtonIndex += 1;
+			}
+		}
+	}
+	
+	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Blue, FString::FromInt(CurrentButtonIndex));
+
+	if (CurrentButtonIndex != PreviousButtonIndex)
+	{
+		ButtonsArray[PreviousButtonIndex]->OnUnhovered.Broadcast();
+		ButtonsArray[CurrentButtonIndex]->OnHovered.Broadcast();
+	}
+
+}
+
+void UMainMenuWidget::CicleArray()
+{
+	for (auto& Button : ButtonsArray)
+	{
+		Button->StartTimer();
+	}
+}
 
--- Source/SpaceShooter/Widgets/MainMenuWidget.h
+++ Source/SpaceShooter/Widgets/MainMenuWidget.h
@@ -4,9 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/InputComponent.h "
 #include "MainMenuWidget.generated.h"
 
-class UButton;
+
+class UMyButton;
+class APlayerController;
 
 /**
  * 
@@ -16,7 +19,46 @@ class SPACESHOOTER_API UMainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
-		UPROPERTY()
-		TArray<UButton> ButtonsArray;
+	virtual void NativeConstruct() override;
 	
+protected:
+
+	UPROPERTY()
+		int32 PreviousButtonIndex = 0;
+
+	UPROPERTY()
+		int32 CurrentButtonIndex = 0;
+
+	UPROPERTY()
+		int32 LastArrayIndex = 0;
+
+	UPROPERTY()
+		APlayerController* PlayerController = nullptr;
+
+	UPROPERTY()
+		TArray<UMyButton*> ButtonsArray;
+	
+	UPROPERTY()
+		UMyButton* NewGameButton = nullptr;
+
+	UPROPERTY()
+		UMyButton* ContinueButton = nullptr;
+
+	UPROPERTY()
+		UMyButton* LoadGameButton = nullptr;
+
+	UPROPERTY()
+		UMyButton* QuitButton = nullptr;
+
+		FInputBinding InputBinding = FInputBinding::FInputBinding();
+ 
+protected:
+
+	void SetupData();
+
+	UFUNCTION()
+		void ScrollButtons(float InValue);
+
+	void CicleArray();
+
 };
